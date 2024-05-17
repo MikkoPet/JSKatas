1da185d7f28d299bd6478210255b87884ada00d9
ShMPt <cacahouettetbs@gmail.com>
2024-05-17 12:55:05 +0200

    feat(07-sum-of-range.js): completed kata

---
+++ katas-js/07-sum-of-range.js
@@ -0,0 +1,32 @@
+"use script";
+
+function range(start, end, step) {
+    if( step === undefined ) {
+        step = 1;
+    }
+    let range = [];
+    let position = 0
+    if (step > 0) {
+    for (start; start <= end; start = start + step) {
+        range[position++] = start;
+    }
+    return range;
+    } else {
+    for (start; start >= end; start = start + step) {
+        range[position++] = start;
+    }
+    return range;
+    }
+} 
+
+console.log(range(5, 2, -1));
+
+function sum(array) {
+    let sum = 0;
+    for (let n = 0; n < array.length; n++) {
+        sum += array[n];
+    }
+    return sum;
+}
+
+console.log(sum(range(1, 10)));
