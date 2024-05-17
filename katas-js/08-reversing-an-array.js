0382033477846e68a761601eb0b91342b9eacb17
ShMPt <cacahouettetbs@gmail.com>
2024-05-17 16:00:41 +0200

    feat(08-reversing-array.js): second half of kata

--- katas-js/08-reversing-an-array.js
+++ katas-js/08-reversing-an-array.js
@@ -11,3 +11,14 @@ function reverseArray(array) {
 
 console.log(reverseArray(["A", "B", "C"]));
 
+
+function reverseArrayInPlace(array) {
+    let runLength = array.length;
+    for (let n = 0 ; n < runLength ; n++){
+        array.unshift(array[n * 2]);
+    }
+    array.length /= 2;
+    return array;
+}
+
+console.log(reverseArrayInPlace(["A", "B", "C"]));
