880ae5d7ec90cf9fd2747c4a15a0d879f6e02c05
ShmPt <cacahouettetbs@gmail.com>
2024-05-19 12:25:39 +0200

    chore: rename reverse kata into evil syntax version

---
+++ katas-js/08-reversing-an-array-evil-syntax.js
@@ -0,0 +1,30 @@
+"use strict";
+
+function reverseArray(array) {
+    let newArray = [];
+    let oldArray = [];
+
+    for (let n in array) {
+        oldArray[n] = array[n];
+    }
+
+    for (let n of array) {
+      newArray.push(oldArray.pop());
+    }
+    
+    return newArray;
+}
+
+console.log(reverseArray(["A", "B", "C"]));
+
+
+function reverseArrayInPlace(array) {
+    for (let n = 0 ; n < ( array.length - n ) ; n++){
+        array.unshift(array[n * 2]);
+    }
+    array.length /= 2;
+    return array;
+}
+
+console.log(reverseArrayInPlace(["A", "B", "C"]));
+
