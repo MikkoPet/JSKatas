f9649ecdf97346e6a4ddbce758cc2b803aa28213
ShmPt <cacahouettetbs@gmail.com>
2024-05-19 12:04:28 +0200

    refacto(02-fizzbuzz.js): change structure to reduce amount of operations in the loop

--- katas-js/02-fizzbuzz.js
+++ katas-js/02-fizzbuzz.js
@@ -1,15 +1,15 @@
 "use strict";
 
-for (let i = 1; i < 20; i++) {
-    if ( i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz");
-    } else if (i % 3 === 0) {
-        console.log("Fizz");
-    } else if (i % 5 === 0) {
-        console.log("Buzz");
-    } else {
-        console.log(i);
-    }
-}
+for (let i = 1; i < 100; i++) {
 
-//go back find better this one's ugly
+    let output = "";
+
+    if ( i % 3 === 0) {
+        output += "Fizz";
+    }
+    if (i % 5 === 0) {
+        output += "Buzz";
+    } 
+
+    console.log(output || i);
+}
