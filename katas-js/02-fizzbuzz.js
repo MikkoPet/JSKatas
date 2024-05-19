76823c40eba01c69e31c3d5fc8d1ad1e4df6b065
ShmPt <cacahouettetbs@gmail.com>
2024-05-19 12:05:16 +0200

    fix(02-fizzbuzz.js): syntax

--- katas-js/02-fizzbuzz.js
+++ katas-js/02-fizzbuzz.js
@@ -4,12 +4,12 @@ for (let i = 1; i < 100; i++) {
 
     let output = "";
 
-    if ( i % 3 === 0) {
+    if ( i % 3 === 0 ) {
         output += "Fizz";
     }
-    if (i % 5 === 0) {
+    if (i % 5 === 0 ) {
         output += "Buzz";
     } 
 
-    console.log(output || i);
+    console.log( output || i );
 }
