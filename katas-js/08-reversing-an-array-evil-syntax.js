13eee0551fa1653f9f223bfab4596f5ce8c073ad
ShmPt <cacahouettetbs@gmail.com>
2024-05-19 12:59:25 +0200

    fix: spacing for readability

--- katas-js/08-reversing-an-array-evil-syntax.js
+++ katas-js/08-reversing-an-array-evil-syntax.js
@@ -19,10 +19,13 @@ console.log(reverseArray(["A", "B", "C"]));
 
 
 function reverseArrayInPlace(array) {
+    
     for (let n = 0 ; n < ( array.length - n ) ; n++){
         array.unshift(array[n * 2]);
     }
+
     array.length /= 2;
+
     return array;
 }
 
